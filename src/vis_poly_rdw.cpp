6efdca6fb4fde40e01d8c4d1c2a1c998efb69fe3
Niall L. Williams <12411434+niallw@users.noreply.github.com>
2022-07-30 21:36:47 -0400

    update/fix visibility-based controller (ismar2021)

--- src/vis_poly_rdw.cpp
+++ src/vis_poly_rdw.cpp
@@ -2,28 +2,14 @@
 #include <fstream>
 #include <chrono>
 
-#include <CGAL/number_utils.h>
-#include <CGAL/Cartesian.h>
-#include "print_utils.h"
-typedef CGAL::Cartesian<double> K;
-typedef CGAL::Aff_transformation_2<K> Transformation;
-typedef CGAL::Point_2<K> Point;
-typedef CGAL::Vector_2<K> Vector;
-typedef CGAL::Direction_2<K> Direction;
-
 #include "vis_poly_rdw.h"
 #include "math.hpp"
-#include "user.hpp"
+#include "user.h"
 #include "geometry.h"
 #include "config.h"
-
-//#include "visibility.hpp"
-#include "vector2.hpp"
+#include "resetter.h"
 
 vis_poly_rdw::vis_poly_rdw(physical_environment* phys_env, virtual_environment* virt_env, resetter* _resetter) {
-	time_t start_time;
-	start_time = time(NULL);
-
 	name = "Vis. Poly. RDW";
 
 	cur_rota_gain = 1.0f;
@@ -39,54 +25,12 @@ vis_poly_rdw::vis_poly_rdw(physical_environment* phys_env, virtual_environment* 
 	cur_curve_per_deg = curve_radius_to_deg_per_meter();
 	curve_dir = 1; // curve to the left of the user == 1. curve to the right of the user == -1
 
-	// Get the angles for distance sampling within the user's field of vision
-	float interval_size = VISUAL_FIELD / NUM_SAMPLE_RAYS;
-	float starting_angle = (math::pi / 2.0f) - (VISUAL_FIELD / 2.0f);
-	sample_directions.push_back(starting_angle);
-	for (int i = 1; i < NUM_SAMPLE_RAYS; i++) {
-		sample_directions.push_back(starting_angle + (interval_size * i));
-	}
-	sample_directions.push_back(VISUAL_FIELD);
-	sample_directions.push_back(FORWARD_ANGLE);
-	sample_directions.push_back(LEFT_ANGLE);
-	sample_directions.push_back(RIGHT_ANGLE);
-
 	reset_policy = _resetter;
-
-	// Get the angles for distance sampling for resetting
-	for (int i = 0; i < NUM_RESET_SAMPLE_RAYS; i++) {
-		reset_sample_directions.push_back(((2 * math::pi) / NUM_RESET_SAMPLE_RAYS) * i);
-	}
 
 	prev_loss = -1.0f;
 	cur_loss = -1.0f;
-
-	cur_alignment = std::vector<float>{ 0.0f, 0.0f, 0.0f };
-	// TODO: need to figure out what to do about this. the cur_alignment variable is a 3-tuple since it's the sample based method that i used initially. but with the vis poly, the alignment is just 1 number (sum of areas of symm diff). idk the best way to fix the code to deal with this. I might just be able to ignore cur_alignment in this class and only care about cur_loss and prev_loss.
-
-	//============================
-	// Gradient stuff
-	for (int i = 0; i < GRADIENT_SAMPLE_RATE; i++) {
-		theta_values.push_back(((2 * math::pi) / GRADIENT_SAMPLE_RATE) * i);
-	}
-
-	//init_vis_poly_data(phys_env, virt_env);
-
-	time_t end_time;
-	end_time = time(NULL);
-	constructor_time += end_time - start_time;
 }
 
-void vis_poly_rdw::init_vis_poly_data(physical_environment* phys_env, virtual_environment* virt_env) {
-	// insert geometry into the arrangement
-	std::vector<Segment_2> verts = phys_env->get_cgal_verts();
-	CGAL::insert_non_intersecting_curves(phys_arr_env, verts.begin(), verts.end());
-
-	verts = virt_env->get_cgal_verts();
-	CGAL::insert_non_intersecting_curves(virt_arr_env, verts.begin(), verts.end());
-}
-
-//visibility_polygon vis_poly_rdw::get_vis_poly(vec2f pos, Arrangement_2 arr_env) {
 visibility_polygon vis_poly_rdw::get_vis_poly(vec2f pos, environment* env, float heading) {
 	namespace V = Visibility;
 	std::vector<V::Segment> segments;
@@ -119,13 +63,6 @@ visibility_polygon vis_poly_rdw::get_vis_poly(vec2f pos, environment* env, float
 }
 
 void vis_poly_rdw::update_visibility_polygons(user* egocentric_user) {
-	time_t start_time;
-	start_time = time(NULL);
-
-	std::chrono::milliseconds start = std::chrono::duration_cast<std::chrono::milliseconds>(
-		std::chrono::system_clock::now().time_since_epoch()
-		);
-
 	virt_vis_poly_center = egocentric_user->state.get_virt_pos();
 	virt_vis_poly_theta = egocentric_user->state.get_virt_heading();
 	//virt_vis_poly = get_vis_poly(virt_vis_poly_center, virt_arr_env);
@@ -139,11 +76,6 @@ void vis_poly_rdw::update_visibility_polygons(user* egocentric_user) {
 	std::chrono::milliseconds end = std::chrono::duration_cast<std::chrono::milliseconds>(
 		std::chrono::system_clock::now().time_since_epoch()
 		);
-
-	int t = end.count() - start.count();
-	time_t end_time;
-	end_time = time(NULL);
-	update_visibility_polygons_time += end_time - start_time;
 }
 
 void vis_poly_rdw::orient_visibility_polygons(user* egocentric_user) {
@@ -197,13 +129,6 @@ void vis_poly_rdw::orient_visibility_polygons(user* egocentric_user) {
 }
 
 redirection_unit vis_poly_rdw::update(float dx, float dy, float dtheta, simulation_state& sim_state, user* egocentric_user) {
-	time_t start_time;
-	start_time = time(NULL);
-	//using namespace std::chrono;
-	std::chrono::milliseconds start = std::chrono::duration_cast<std::chrono::milliseconds>(
-		std::chrono::system_clock::now().time_since_epoch()
-		);
-
 	//update_dynamic_obstacles(); // TODO: add support for dyunamic obstacles, and update the vertices of those obstalces in the environment objects
 	update_visibility_polygons(egocentric_user);
 	//orient_visibility_polygons(egocentric_user);
@@ -211,19 +136,10 @@ redirection_unit vis_poly_rdw::update(float dx, float dy, float dtheta, simulati
 	set_steer_target(sim_state, egocentric_user);
 
 	update_loss(sim_state, egocentric_user);
-	cur_alignment = std::vector<float>{ cur_north_loss, cur_east_loss, cur_west_loss };
 
-	redirection_unit next_redirection = set_gains(dx, dy, dtheta, sim_state, egocentric_user, new node(vec2f(0.0f, 0.0f)));
+	redirection_unit next_redirection = set_gains(dx, dy, dtheta, sim_state, egocentric_user);
 
 	prev_loss = cur_loss;
-
-	time_t end_time;
-	end_time = time(NULL);
-	std::chrono::milliseconds end = std::chrono::duration_cast<std::chrono::milliseconds>(
-		std::chrono::system_clock::now().time_since_epoch()
-		);
-	//update_time += end_time - start_time;
-	update_time += end.count() - start.count();
 	return next_redirection;
 }
 
@@ -248,64 +164,15 @@ void vis_poly_rdw::set_steer_target(simulation_state& sim_state, user* egocentri
 	steer_target = egocentric_user->state.get_phys_pos() + best_slice->bisector;
 }
 
-float vis_poly_rdw::get_symm_diff_area(slice* phys_slice, slice* virt_slice) {
-	vec2f origin = vec2f(0.0f, 0.0f);
-	Polygon_2 phys_p;
-	for (vec2f* v : *(phys_slice->pts)) {
-		float offset = signed_angle(phys_slice->bisector, vec2f(1.0f, 0.0f));
-		vec2f rotated = rotate_around(origin, *v, offset);
-		phys_p.push_back(Point_2(rotated.x, rotated.y));
-	}
-
-	Polygon_2 virt_p;
-	for (vec2f* v : *(virt_slice->pts)) {
-		float offset = signed_angle(virt_slice->bisector, vec2f(1.0f, 0.0f));
-		vec2f rotated = rotate_around(origin, *v, offset);
-		virt_p.push_back(Point_2(rotated.x, rotated.y));
-	}
-
-	Pwh_list_2 symmR;
-	CGAL::symmetric_difference(phys_p, virt_p, std::back_inserter(symmR));
-	float outer_area = 0.0f;
-	float inner_area = 0.0f;
-	
-	for (auto it = symmR.begin(); it != symmR.end(); ++it) {
-		auto test = (*it).outer_boundary();
-		auto test2 = test.area();
-		outer_area += (float)to_double(test2);
-
-		int k = 1;
-		for (auto hit = (*it).holes_begin(); hit != (*it).holes_end(); ++hit, ++k){
-			std::vector<vec2f*> pts;
-			for (auto vit = (*hit).vertices_begin(); vit != (*hit).vertices_end(); ++vit) {
-				pts.insert(pts.begin(), new vec2f((float)to_double((*vit).x()),
-					(float)to_double((*vit).y())));
-			}
-			inner_area += geom::polygon_area(pts);
-		}
-	}
-
-	return outer_area - inner_area;
-}
-
 void vis_poly_rdw::update_loss(simulation_state& sim_state, user* egocentric_user) {
-	time_t start_time;
-	start_time = time(NULL);
 	cur_north_loss = 0.0f;
 	cur_east_loss = 0.0f;
 	cur_west_loss = 0.0f;
 
-	cur_loss = phys_vis_poly - virt_vis_poly;
-
-	time_t end_time;
-	end_time = time(NULL);
-	update_loss_time += end_time - start_time;
+	cur_loss = 0.0f;
 }
 
 float vis_poly_rdw::get_proximity_in_visibility_polygon(float dir, std::vector<vec2f> poly) {
-	time_t start_time;
-	start_time = time(NULL);
-
 	float best_dist = math::max_float;
 	vec2f vec_dir = rad_2_vec(dir);
 	
@@ -315,17 +182,10 @@ float vis_poly_rdw::get_proximity_in_visibility_polygon(float dir, std::vector<v
 		float d = geom::ray_line_intersect(new vec2f(0.0f, 0.0f), &vec_dir, &p1, &p2); // Ray origin is (0, 0) because the visibility polygon has been recentered.
 		if (d != -1.0f && d < best_dist) best_dist = d;
 	}
-
-	time_t end_time;
-	end_time = time(NULL);
-	get_proximity_in_visibility_polygon_time += end_time - start_time;
 	return best_dist;
 }
 
-redirection_unit vis_poly_rdw::set_gains(float dx, float dy, float dtheta, simulation_state& sim_state, user* egocentric_user, node* optimal_node) {
-	time_t start_time;
-	start_time = time(NULL);
-
+redirection_unit vis_poly_rdw::set_gains(float dx, float dy, float dtheta, simulation_state& sim_state, user* egocentric_user) {
 	redirection_unit redir_unit;
 	vec2f user_dir = rad_2_vec(egocentric_user->state.get_phys_heading());
 	vec2f user_to_target = normalize(steer_target - egocentric_user->state.get_phys_pos());
@@ -401,30 +261,17 @@ redirection_unit vis_poly_rdw::set_gains(float dx, float dy, float dtheta, simul
 	cur_curve_gain = redir_unit.curve_gain;
 	curve_dir = redir_unit.curve_gain_dir;
 
-	time_t end_time;
-	end_time = time(NULL);
-	set_gains_time += end_time - start_time;
 	return redir_unit;
 }
 
 float vis_poly_rdw::set_rotation_gain(simulation_state& sim_state, user* egocentric_user) {
-	//todo
-	//float virt_distance_north = cur_virt_vis_poly.distance_north;
-	//float phys_distance_north = cur_phys_vis_poly.distance_north;
-
-	//float trans_gain = p/*hys_distance_north / virt_distance_north;
-	//trans_gain = math::clamp(trans_gain, min_trans_gain, max_trans_gain);
-	//return trans_gain;*/
 	return 0.0f;
 }
 
 float vis_poly_rdw::set_translation_gain(simulation_state& sim_state, user* egocentric_user) {
-
 	float trans_gain = phys_north_loss / virt_north_loss;
 	trans_gain = math::clamp(trans_gain, min_trans_gain, max_trans_gain);
 
-	//if (trans_gain < 1.0f) trans_gain = min_trans_gain;
-	//if (trans_gain > 1.0f) trans_gain = max_trans_gain;
 	return trans_gain;
 }
 
@@ -437,26 +284,9 @@ float vis_poly_rdw::set_curvature_gain(int direction) {
 		scale = math::min(1.0f, cur_east_loss);
 	}
 	return math::min(cur_curve_per_deg * math::abs(scale), cur_curve_per_deg);
-
-	/*
-	float min_val = math::min(cur_west_loss, cur_east_loss);
-	float max_val = math::max(cur_west_loss, cur_east_loss);
-
-	// Avoid division by zero
-	if (min_val) {
-		float ratio = (math::abs(max_val) / math::abs(min_val)) - 1.0f;
-		return math::min(cur_curve_per_deg * ratio, cur_curve_per_deg);
-	}
-	else {
-		return cur_curve_per_deg;
-	}
-	*/
 }
 
 float vis_poly_rdw::distance_to_closest_feature(vec2f pos, vec2f dir, visibility_polygon vis_poly) {
-	time_t start_time;
-	start_time = time(NULL);
-
 	float min_dist = math::max_float;
 	for (int i = 0; i < vis_poly.verts.size(); i++) {
 		vec2f* p1 = vis_poly.verts[i % vis_poly.verts.size()];
@@ -467,74 +297,10 @@ float vis_poly_rdw::distance_to_closest_feature(vec2f pos, vec2f dir, visibility
 		}
 	}
 
-	time_t end_time;
-	end_time = time(NULL);
-	distance_to_closest_feature_time += end_time - start_time;
 	return min_dist;
 }
 
 std::vector<std::vector<vec2f>> vis_poly_rdw::get_gradient_data(physical_environment* phys_env, std::deque<proximity_container*> prox_queue) {
 	std::vector<std::vector<vec2f>> test;
 	return test;
-}
-
-void vis_poly_rdw::reset_to_gradient(vec2f phys_heading, float virt_heading, std::vector<trajectory_unit>* path, std::deque<proximity_container*> prox_queue, physical_environment* phys_env, vec2f phys_pos, vec2f virt_pos) {
-	time_t start_time;
-	start_time = time(NULL);
-
-	gradient_dir = compute_gradient(phys_env, prox_queue, phys_pos);
-	reorient_to_target(phys_heading, virt_heading, phys_pos, virt_pos, gradient_dir + phys_pos, path);
-
-	time_t end_time;
-	end_time = time(NULL);
-	reset_to_gradient_time += end_time - start_time;
-}
-
-vec2f vis_poly_rdw::compute_gradient(physical_environment* phys_env, std::deque<proximity_container*> prox_queue, vec2f phys_pos) {
-	time_t start_time;
-	start_time = time(NULL);
-
-	std::vector<object*> objects;
-	for (proximity_container* c : prox_queue) {
-		if (c->get_obstacle()->space == object::SPACE_TYPE::PHYS) {
-			objects.push_back(c->get_obstacle());
-		}
-	}
-	float cur_repulsive_force = compute_repulsive_force(phys_pos, objects);
-
-	float min_force = std::numeric_limits<float>::max();
-	vec2f min_force_pos;
-	for (float theta : theta_values) {
-		vec2f new_pos = phys_pos + (rad_2_vec(theta) * SFR2G_STEP_SIZE);
-		if (!phys_env->point_is_legal(new_pos)) continue;
-
-		float new_force = compute_repulsive_force(new_pos, objects);
-		if (new_force <= min_force) {
-			min_force = new_force;
-			min_force_pos = new_pos;
-		}
-	}
-
-	assert(min_force != std::numeric_limits<float>::max());
-
-	time_t end_time;
-	end_time = time(NULL);
-	compute_gradient_time += end_time - start_time;
-	return normalize(min_force_pos - phys_pos);
-}
-
-float vis_poly_rdw::compute_repulsive_force(vec2f pos, std::vector<object*> objects) {
-	time_t start_time;
-	start_time = time(NULL);
-
-	float dist_sum = 0.0f;
-	for (object* obj : objects) {
-		if (obj->space == object::SPACE_TYPE::PHYS)
-			dist_sum += 1.0f / obj->distance(pos);
-	}
-
-	time_t end_time;
-	end_time = time(NULL);
-	compute_repulsive_force_time += end_time - start_time;
-	return dist_sum;
 }
--- src/visibility_polygon.cpp
+++ src/visibility_polygon.cpp
@@ -1,5 +1,4 @@
 #include "visibility_polygon.h"
-#include "polygon_match.h"
 #include "obstacle.h"
 #include "wall.h"
 #include "math.hpp"
@@ -12,45 +11,20 @@ visibility_polygon::visibility_polygon() {
 
 visibility_polygon::visibility_polygon(std::vector<vec2f*> vertices, vec2f* center_pos, float _heading) {
 	boundary_pts = vertices;
-	//verts = vertices;
 	center = center_pos;
 	for (vec2f* v : vertices) {
 		verts.push_back(new vec2f(v->x - center_pos->x, v->y - center_pos->y));
 	}
 	this->heading = _heading;
-	distance_north = math::max_float;
-	distance_east = math::max_float;
-	distance_south = math::max_float;
-	distance_west = math::max_float;
 	compute_slices();
-	flag = 0;
-	//compute_features();
 	//simplify();
-	//normalize_polygon();
-	//compute_turning_function();
 }
 
 visibility_polygon::visibility_polygon(environment* env_ptr, vec2f* center_pos) {
 	env = env_ptr;
 	center = center_pos;
-	distance_north = math::max_float;
-	distance_east = math::max_float;
-	distance_south = math::max_float;
-	distance_west = math::max_float;
-	flag = 1;
-	//compute_features();
 	//simplify();
-	//normalize_polygon();
-	//compute_turning_function();
 }
-
-//visibility_polygon::~visibility_polygon() {
-	//for (auto v : verts) {
-	//for (int i = 0; i < verts.size(); i++){
-		//delete verts[i];
-		//delete v;
-	//}
-//}
 
 void visibility_polygon::simplify() {
 	std::vector<vec2f*> new_verts;
@@ -77,21 +51,12 @@ void visibility_polygon::simplify() {
 	boundary_pts = new_verts;
 }
 
-void visibility_polygon::add_heading(float dir) {
-	this->heading = fmod(dir, math::two_pi);
-}
-
 void visibility_polygon::compute_slices() {
-	simple_slice();
-	//smart_slice();
-}
-
-void visibility_polygon::simple_slice() {
 	for (int i = 0; i < verts.size(); i++) {
 		vec2f* p1 = verts[i];
 		vec2f* p2 = verts[(i + 1) % verts.size()];
 		if (geom::orient(p1, p2, &(vec2f(0, 0))) == 0 ||
-			signed_angle(normalize(*p1), normalize(*p2)) <= SIMPLE_SLICE_THETA_THRESHOLD) continue; // colinear
+			signed_angle(normalize(*p1), normalize(*p2)) <= SLICE_THETA_THRESHOLD) continue; // colinear
 		std::vector<vec2f*>* arr = new std::vector<vec2f*>;
 		arr->push_back(new vec2f(*p1));
 		arr->push_back(new vec2f(*p2));
@@ -100,237 +65,3 @@ void visibility_polygon::simple_slice() {
 	std::sort(slices.begin(), slices.end(), compare_by_offset());
 }
 
-void visibility_polygon::smart_slice() {
-	int start_index = 0;
-	for (int end_index = 0; end_index < verts.size(); end_index++) {
-		vec2f* p1 = verts[start_index];
-		vec2f* p2 = verts[end_index % verts.size()];
-		if (geom::orient(p1, p2, &(vec2f(0, 0))) == 0) continue; // colinear
-
-		float angle_between_pts = angle(normalize(*p1), normalize(*p2));
-		if (math::abs(angle_between_pts) < SLICE_THETA_THRESHOLD) continue; // Slice is too narrow
-
-		std::vector<vec2f*>* arr = new std::vector<vec2f*>;
-		for (int i = start_index; i <= end_index; i++) {
-			arr->push_back(new vec2f(*(verts[i])));
-		}
-		slices.push_back(new slice(arr, heading));
-		start_index = end_index;
-	}
-	// Check the last slice manually
-	vec2f* p1 = verts[start_index];
-	vec2f* p2 = verts[0];
-	float angle_between_pts = angle(normalize(*p1), normalize(*p2));
-	if (geom::orient(p1, p2, &(vec2f(0, 0))) != 0 &&
-		math::abs(angle_between_pts) >= SLICE_THETA_THRESHOLD) {
-		std::vector<vec2f*>* arr = new std::vector<vec2f*>;
-		arr->push_back(new vec2f(*p1));
-		arr->push_back(new vec2f(*p2));
-		slices.push_back(new slice(arr, heading));
-	}
-	std::sort(slices.begin(), slices.end(), compare_by_offset());
-}
-
-void visibility_polygon::compute_features() {
-	num_vertices = verts.size();
-	area = geom::polygon_area(verts);
-	perimeter = geom::polygon_perimeter(verts);
-	min_dist = get_min_dist();
-	max_dist = get_max_dist();
-	avg_dist = area / perimeter;
-	jaggedness = (perimeter * perimeter) / area;
-	vertex_density = num_vertices / area;
-}
-
-float visibility_polygon::get_min_dist() {
-	float best_dist = math::max_float;
-
-	for (int i = 0; i < boundary_pts.size(); i++) {
-		vec2f* p1 = boundary_pts[i];
-		vec2f* p2 = boundary_pts[(i + 1) % boundary_pts.size()];
-		float d = geom::line_point_distance(p1, p2, center);
-		if (d < best_dist) best_dist = d;
-	}
-
-	return best_dist;
-}
-
-float visibility_polygon::get_max_dist() {
-	float best_dist = -1.0f;
-
-	for (int i = 0; i < boundary_pts.size(); i++) {
-		vec2f* p1 = boundary_pts[i];
-		float d = length(*center - *p1);
-		if (d >= best_dist) best_dist = d;
-	}
-
-	return best_dist;
-}
-
-void visibility_polygon::normalize_polygon() {
-	float perimeter = 0.0f;
-	for (int i = 0; i < boundary_pts.size(); i++) {
-		vec2f* p1 = boundary_pts[i];
-		vec2f* p2 = boundary_pts[(i+1) % boundary_pts.size()];
-		perimeter += length(*p1 - *p2);
-	}
-	normalized_verts.push_back(vec2f(0.0f, 0.0f));
-	for (int i = 0; i < boundary_pts.size() - 1; i++) {
-		vec2f* p1 = boundary_pts[i];
-		vec2f* p2 = boundary_pts[(i + 1) % boundary_pts.size()];
-		vec2f seg = *p2 - *p1;
-		float seg_length = length(seg);
-		vec2f new_seg = normalize(seg) * (seg_length / perimeter);
-		normalized_verts.push_back(normalized_verts[i] + new_seg);
-	}
-}
-
-polygon_similarity visibility_polygon::compare(visibility_polygon other_poly) {
-	poly poly_f, poly_g;
-	TURN_REP_REC trf, trg;
-	TURN_REP f, g;
-	POLY_REC pf, pg;
-	EVENT_REC e;
-	double ht0, slope, alpha, theta_star, metric2, metric, ht0_err, slope_err;
-	int update_p = 1;
-	int precise_p = 0;
-	int i = 0;
-	while (i < verts.size()) {
-		poly_f.pt[i].x = verts[i]->x;
-		poly_f.pt[i].y = verts[i]->y;
-		i++;
-	}
-	poly_f.n = i;
-
-	i = 0;
-	while (i < other_poly.verts.size()) {
-		poly_g.pt[i].x = other_poly.verts[i]->x;
-		poly_g.pt[i].y = other_poly.verts[i]->y;
-		i++;
-	}
-	poly_g.n = i;
-
-	poly_to_turn_rep(&poly_g, &trg);
-	g = &trg;
-	poly_to_turn_rep(&poly_f, &trf);
-	f = &trf;
-
-	init_vals(f, g, &ht0, &slope, &alpha);
-	init_events(f, g);
-	metric2 = h_t0min(f, g, ht0, slope, alpha, update_p ? reinit_interval(f, g) : 0, &theta_star, &e, &ht0_err, &slope_err);
-	metric = metric2 > 0 ? sqrt(metric2) : 0;
-
-	return polygon_similarity(metric, turn(theta_star, 0));
-}
-
-visibility_polygon visibility_polygon::rotate(vec2f rotation_origin, float theta) {
-	std::vector<vec2f*> rotated_verts;
-	for (vec2f* v : boundary_pts) {
-		rotated_verts.push_back(new vec2f(rotate_around(rotation_origin, *v, theta)));
-	}
-	return visibility_polygon(rotated_verts, center, 0.0f);
-}
-
-float visibility_polygon::compute_distance_loss(visibility_polygon* virt_poly, float phys_heading, float virt_heading) {
-	this->compute_nearest_features(rad_2_vec(phys_heading));
-	virt_poly->compute_nearest_features(rad_2_vec(virt_heading));
-
-	float sum = 0.0f;
-	sum += math::abs(distance_north - virt_poly->distance_north);
-	sum += math::abs(distance_east - virt_poly->distance_east);
-	sum += math::abs(distance_south - virt_poly->distance_south);
-	sum += math::abs(distance_west - virt_poly->distance_west);
-	return sum;
-}
-
-/*
-THIS IS THE VERSION OF THE FUNCTION THAT WORKED FOR WHEN I WAS COMPUTING THE VISIBILITY POLYGON. IT IS STILL USEFUL IF I WANT TO SWITCH BACK TO THAT VERSION OF THE CODE.
-void visibility_polygon::compute_nearest_features(vec2f heading) {
-	std::vector<vec2f> directions{ heading, vec2f(heading.y, -heading.x), vec2f(-heading.x, -heading.y), vec2f(-heading.y, heading.x) };
-
-	for (int i = 0; i < directions.size(); i++) {
-		for (int j = 0; j < verts.size(); j++) {
-			vec2f p1 = verts[j];
-			vec2f p2 = verts[(j + 1) % verts.size()];
-			float t = geom::ray_line_intersect(center, directions[i], p1, p2);
-			if (t >= 0) {
-				switch (i) {
-				case 0: {
-					//if (t < distance_north) 
-					distance_north = t;
-					break;
-				}
-				case 1: {
-					//if (t < distance_east) 
-					distance_east = t;
-					break;
-				}
-				case 2: {
-					//if (t < distance_south) 
-					distance_south = t;
-					break;
-				}
-				case 3: {
-					//if (t < distance_west) 
-					distance_west = t;
-					break;
-				}
-				}
-			}
-			if (t >= 0) {
-				//break;
-			}
-
-		}
-	}
-}
-*/
-
-void visibility_polygon::compute_nearest_features(vec2f heading) {
-	std::vector<vec2f> directions{ heading, vec2f(heading.y, -heading.x), vec2f(-heading.x, -heading.y), vec2f(-heading.y, heading.x) };
-
-	for (int i = 0; i < directions.size(); i++) {
-		float closest = math::max_float;
-		for (int j = 0; j < boundary_pts.size(); j++) {
-			vec2f* p1 = boundary_pts[j];
-			vec2f* p2 = boundary_pts[(j+1)%boundary_pts.size()];
-			float t = geom::ray_line_intersect(center, new vec2f(directions[i]), p1, p2);
-			if (t >= 0 && t < closest) {
-				closest = t;
-			}
-		}
-
-		// Assign the distance value to the correct direction.
-		switch (i) {
-		case 0: {
-			distance_north = closest;
-			break;
-		}
-		case 1: {
-			distance_east = closest;
-			break;
-		}
-		case 2: {
-			distance_south = closest;
-			break;
-		}
-		case 3: {
-			distance_west = closest;
-			break;
-		}
-		}
-	}
-}
-
-float visibility_polygon::operator-(const visibility_polygon& o) {
-	float sum = 0.0f;
-	sum += math::abs(this->num_vertices - o.num_vertices);
-	sum += math::abs(this->area - o.area);
-	sum += math::abs(this->perimeter - o.perimeter);
-	sum += math::abs(this->min_dist - o.min_dist);
-	sum += math::abs(this->max_dist - o.max_dist);
-	sum += math::abs(this->avg_dist - o.avg_dist);
-	sum += math::abs(this->jaggedness - o.jaggedness);
-	sum += math::abs(this->vertex_density - o.vertex_density);
-	return sum;
-}
