010944696735eb444beff605f2bfbae09fe2b3fb
Niall L. Williams <12411434+niallw@users.noreply.github.com>
2022-07-30 21:36:16 -0400

    update/fix visibility-based controller (ismar2021)

--- include/vis_poly_rdw.h
+++ include/vis_poly_rdw.h
@@ -1,111 +1,35 @@
 #pragma once
 
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Boolean_set_operations_2.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Triangular_expansion_visibility_2.h>
-#include <CGAL/Rotational_sweep_visibility_2.h>
-#include <CGAL/Arr_segment_traits_2.h>
-#include <CGAL/Arrangement_2.h>
-// For symmetric difference
-typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
-typedef Kernel::Point_2                                   Point_2;
-typedef CGAL::Polygon_2<Kernel>                           Polygon_2;
-typedef CGAL::Polygon_with_holes_2<Kernel>                Polygon_with_holes_2;
-typedef std::list<Polygon_with_holes_2>                   Pwh_list_2;
-
-// For visibility polygon
-typedef Kernel::Segment_2                                       Segment_2;
-typedef CGAL::Arr_segment_traits_2<Kernel>                      Traits_2;
-typedef CGAL::Arrangement_2<Traits_2>                           Arrangement_2;
-typedef Arrangement_2::Halfedge_const_handle                    Halfedge_const_handle;
-typedef Arrangement_2::Face_handle                              Face_handle;
-// Define the used visibility class
-typedef CGAL::Triangular_expansion_visibility_2<Arrangement_2>  TEV;
-typedef CGAL::Rotational_sweep_visibility_2<Arrangement_2, CGAL::Tag_false> RSV;
-
 #include "redirector.h"
-#include "graph.h"
+#include "resetter.h"
 #include "visibility_polygon.h"
 #include "VisibilityPolygon.h"
 
 class vis_poly_rdw : public redirector {
 public:
 	vis_poly_rdw(physical_environment* phys_env, virtual_environment* virt_env, resetter* _resetter);
-	void build_visibility_graphs(physical_environment* phys_env, virtual_environment* virt_env);
 	redirection_unit update(float dx, float dy, float dtheta, simulation_state& sim_state, user* egocentric_user);
 	std::vector<std::vector<vec2f>> get_gradient_data(physical_environment* phys_env, std::deque<proximity_container*> prox_queue);
 
 	visibility_polygon phys_vis_poly;
-	std::vector<vec2f> simple_phys_vis_poly;
 	vec2f phys_vis_poly_center; // The point of the observer in the vis poly
 	float phys_vis_poly_theta; // The user's orientation inside the vis poly
 	visibility_polygon virt_vis_poly;
-	std::vector<vec2f> simple_virt_vis_poly;
 	vec2f virt_vis_poly_center; // The point of the observer in the vis poly
 	float virt_vis_poly_theta; // The user's orientation inside the vis poly
-	std::vector<float> sample_directions;
-	std::vector<float> reset_sample_directions;
-
-	float constructor_time = 0.0f;
-	float get_vis_poly_time = 0.0f;
-	float update_visibility_polygons_time = 0.0f;
-	float update_time = 0.0f;
-	float get_loss_gradient_direction_time = 0.0f;
-	float get_simple_visibility_polygons_time = 0.0f;
-	float update_loss_time = 0.0f;
-	float get_ray_weight_time = 0.0f;
-	float get_proximity_in_visibility_polygon_time = 0.0f;
-	float area_of_polygon_with_holes_time = 0.0f;
-	float compute_rotation_loss_time = 0.0f;
-	float set_gains_time = 0.0f;
-	float reset_time = 0.0f;
-	float reset_to_distance_alignment_time = 0.0f;
-	float get_current_visibility_polygon_time = 0.0f;
-	float distance_to_closest_feature_time = 0.0f;
-	float reset_to_gradient_time = 0.0f;
-	float compute_gradient_time = 0.0f;
-	float compute_repulsive_force_time = 0.0f;
-	slice test;
 
 private:
-	redirection_unit set_gains(float dx, float dy, float dtheta, simulation_state& sim_state, user* egocentric_user, node* optimal_node);
+	redirection_unit set_gains(float dx, float dy, float dtheta, simulation_state& sim_state, user* egocentric_user);
 	float set_rotation_gain(simulation_state& sim_state, user* egocentric_user);
 	float set_translation_gain(simulation_state& sim_state, user* egocentric_user);
 	float set_curvature_gain(int direction);
 	float distance_to_closest_feature(vec2f pos, vec2f dir, visibility_polygon vis_poly);
 	void update_loss(simulation_state& sim_state, user* egocentric_user);
 	void update_visibility_polygons(user* egocentric_user);
-	//visibility_polygon get_vis_poly(vec2f pos, Arrangement_2 arr_env);
 	visibility_polygon get_vis_poly(vec2f pos, environment* env, float heading);
 	void orient_visibility_polygons(user* egocentric_user);
 	float get_proximity_in_visibility_polygon(float dir, std::vector<vec2f> poly);
-	void init_vis_poly_data(physical_environment* phys_env, virtual_environment* virt_env);
 	void set_steer_target(simulation_state& sim_state, user* egocentric_user);
-	float get_symm_diff_area(slice* phys_slice, slice* virt_slice);
-
-	//============================
-	// Gradient stuff
-	void reset_to_gradient(vec2f phys_heading, float virt_heading, std::vector<trajectory_unit>* path, std::deque<proximity_container*> prox_queue, physical_environment* phys_env, vec2f phys_pos, vec2f virt_pos);
-	vec2f compute_gradient(physical_environment* phys_env, std::deque<proximity_container*> prox_queue, vec2f phys_pos);
-	float compute_repulsive_force(vec2f pos, std::vector<object*> objects);
-	std::vector<float> theta_values;
-	float SFR2G_STEP_SIZE = 0.1f;
-	const int GRADIENT_SAMPLE_RATE = 8; // Number of points around the user to check the value of the potential field
-	vec2f gradient_dir;
-	//============================
-
-	const float PREDICTION_TIME_HORIZON = 1.0f;
-	const float REDIRECTION_DISTANCE_THRESHOLD = 0.25f;
-	const int NUM_RESET_SAMPLE_RAYS = 20;
-	const float FORWARD_DISTANCE_WEIGHT = 5.0f;
-	const float REDIRECTION_LOSS_THRESHOLD = 0.1f;
-	const float ROTA_GAIN_SMOOTHING = 0.125f;
-	const float VISUAL_FIELD = math::radians(210.0f); // radians
-	const float FORWARD_ANGLE = math::pi / 2;
-	const float LEFT_ANGLE = math::pi;
-	const float RIGHT_ANGLE = 0.0f;
-	const int NUM_SAMPLE_RAYS = 10; // Excluding the extents of the visual field.
 
 	vec2f loss_gradient_dir;
 	float prev_loss;
@@ -123,8 +47,6 @@ private:
 	float cur_east_loss;
 	float cur_west_loss;
 	float prev_rota_gain;
-	Arrangement_2 phys_arr_env;
-	Arrangement_2 virt_arr_env;
 
 	vec2f steer_target;
 	slice* best_slice;
--- include/visibility_polygon.h
+++ include/visibility_polygon.h
@@ -4,18 +4,18 @@
 
 class environment;
 
-#include "vec2f.hpp"
+#include "vec2f.h"
 #include "geometry.h"
 #include "polygon.h"
 
 //#include "math.hpp"
 //#include <CGAL/Point_2.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/number_utils.h>
-#include <CGAL/Cartesian.h>
+//#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+//#include <CGAL/number_utils.h>
+//#include <CGAL/Cartesian.h>
 //typedef CGAL::Exact_predicates_exact_constructions_kernel       Kernel;
 //typedef Kernel::Point_2                                         Point_2;
-typedef CGAL::Point_2<CGAL::Cartesian<double>> Point;
+//typedef CGAL::Point_2<CGAL::Cartesian<double>> Point;
 
 struct vis_poly_edge;
 struct vis_poly_vertex;
@@ -73,27 +73,6 @@ struct vis_poly_edge {
 	vis_poly_vertex* p2;
 };
 
-struct polar_coord_comparator {
-	inline bool operator() (const vis_poly_vertex* p1, const vis_poly_vertex* p2) {
-		if (p1->theta == p2->theta) {
-			if (length(p1->cartesian - p1->origin) != length(p2->cartesian - p2->origin)) {
-				return length(p1->cartesian - p1->origin) < length(p2->cartesian - p2->origin);
-			}
-			if (p1->r == p2->r) {
-				if (p1->is_first && !p2->is_first) {
-					return false;
-				}
-				else if ((!p1->is_first && !p2->is_first) ||
-					(p1->is_first && p2->is_first)) {
-					return false;
-				}
-				else return true;
-			}
-		}
-		return (p1->theta < p2->theta);
-	}
-};
-
 struct polygon_similarity {
 	polygon_similarity();
 
@@ -121,8 +100,6 @@ struct slice {
 
 		p1_theta = signed_angle(rad_2_vec(heading), normalize(*p1));
 		p2_theta = signed_angle(rad_2_vec(heading), normalize(*p2));
-		float test = signed_angle(normalize(*p1), normalize(*p2));
-		bool t = CGAL::collinear(CGAL::Exact_predicates_inexact_constructions_kernel::Point_2(0.0f, 0.0f), CGAL::Exact_predicates_inexact_constructions_kernel::Point_2(p1->x, p1->y), CGAL::Exact_predicates_inexact_constructions_kernel::Point_2(p2->x, p2->y));
 
 		vec2f p1_to_p2 = *p2 - *p1;
 		bisector = normalize(*p1 + (p1_to_p2 * 0.5f));
@@ -165,17 +142,7 @@ class visibility_polygon : public polygon {
 		visibility_polygon(environment* env_ptr, vec2f* center);
 		//~visibility_polygon();
 		void simplify();
-		polygon_similarity compare(visibility_polygon other_poly);
-		visibility_polygon rotate(vec2f rotation_origin, float theta);
-		float compute_distance_loss(visibility_polygon* virt_poly, float phys_heading, float virt_heading);
-		void compute_nearest_features(vec2f heading);
-		float operator-(const visibility_polygon& o);
-		void add_heading(float dir);
 		void compute_slices();
-
-		void compute_features();
-		float get_min_dist();
-		float get_max_dist();
 
 		std::vector<vec2f*> verts;
 		std::vector<slice*> slices;
@@ -186,26 +153,8 @@ class visibility_polygon : public polygon {
 		int num_vertices;
 		float area;
 		float perimeter;
-		float min_dist;
-		float max_dist;
-		float avg_dist;
-		float jaggedness;
-		float vertex_density;
 		float heading;
 
-		float distance_north;
-		float distance_east;
-		float distance_south;
-		float distance_west;
-		int flag;
-
 	private:
-		void normalize_polygon();
-		void simple_slice();
-		void smart_slice();
-
-		std::vector<vec2f> normalized_verts;
-		std::vector<std::pair<float, float>> turning_func;
-		float SLICE_THETA_THRESHOLD = 0.523599f; // 30 degrees
-		float SIMPLE_SLICE_THETA_THRESHOLD = 0.0174533f; // 30 degrees
+		float SLICE_THETA_THRESHOLD = 0.0174533f; // 30 degrees
 };
